9be158013cf04e0f9c41054b7ea7750b4612b0d0
MohanRajA9 <akmohanraj555@gmail.com>
2024-03-02 17:05:18 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
 import Sidebar from './components/Sidebar';
+import Dashboard from './components/Dashboard';
+import Topbar from './components/Topbar';
+import User from './components/User'
 
 function App() {
   return (
-    <div className="App">
+      <BrowserRouter>
+     <div className="App">
       <div id="wrapper">
-        <Sidebar/>
+      <Sidebar/>
+      <div id="content-wrapper" className="d-flex flex-column">
+      <div id="content">
+        <Topbar/>
+        <div className="container-fluid">
+        <Switch>
+         <Route exact={true} path="/" component={ Dashboard } />
+         <Route exact={true} path="/user" component={ User } />
+        </Switch>
+        </div>
       </div>
-    </div>
-  );
+      </div>
+      </div>
+     </div>
+    </BrowserRouter>
+ );
 }
 
 export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+function Cards () {
+    const data = [
+        {
+            title : "Earnings (Monthly)",
+            price : "$40,000",
+            color : "primary",
+            icon : "fas fa-calendar"
+        },
+        {
+            title : "Earnings (Annual)",
+            price : "$215,000",
+            color : "success",
+            icon : "fas fa-dollar-sign"
+        },
+        {
+            title : "Tasks",
+            price : "50%",
+            color : "info",
+            icon : "fas fa-clipboard-list"
+        },
+        {
+            title : "Pending Requests",
+            price : "18",
+            color : "warning",
+            icon : "fas fa-comments"
+        },
+    ]
+    return(
+            <div className="row">
+                {data.map((item, index)=>{ return(
+                                            <div key={index} className="col-xl-3 col-md-6 mb-4">
+                                            <div className={`card border-left-${item.color} shadow h-100 py-2`}>
+                                                <div className="card-body">
+                                                    <div className="row no-gutters align-items-center">
+                                                        <div className="col mr-2">
+                                                            <div className={`text-xs font-weight-bold text-${item.color} text-uppercase mb-1`}>
+                                                                {item.title}</div>
+                                                            <div className="h5 mb-0 font-weight-bold text-gray-800">{item.price}</div>
+                                                        </div>
+                                    <div className="col-auto">
+                                         <i className={`${item.icon} fa-2x text-gray-300`}></i>
+                                    </div>                  
+                                </div>
+                             </div>
+                         </div>
+                     </div>
+                  )}
+                )}
+
+            </div>
+          )
+}
+
+export default Cards
